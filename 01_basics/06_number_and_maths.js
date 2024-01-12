401075b4233d45a9d00686d7468250efce000a24
Harrsha T <142029312+harrshat@users.noreply.github.com>
2024-01-12 11:02:37 +0000

    Numbers and Math

---
+++ 01_basics/06_number_and_maths.js
@@ -0,0 +1,24 @@
+const score = 400;
+console.log(score);
+
+const balance = new Number(100);
+console.log(balance);
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+
+const otherNumber = 23.8966
+console.log(otherNumber.toPrecision(5));
+
+// To display numbers according to the country mentioned within the brackets
+const hundreds = 100000;
+console.log(hundreds.toLocaleString('en-IN'));
+
+// +++++++++++++ Maths ++++++++++++
+console.log(Math);
+console.log(Maths.abs(-5));
+console.log(Math.round(5.4353254));
+
+
+
+
