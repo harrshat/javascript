8fe5a8e47b28f7da76fc7c7cff77f4329e3ccf1f
Harrsha T <142029312+harrshat@users.noreply.github.com>
2024-01-12 05:50:44 +0000

    New files added

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 144553
+let accountEmail = "harrsha.work@gmail.com"
+var accountPassword = "12345"
+accountCity = "Chennai"
+
+//var isn't used because of issues with block scope
+//accountId = 2 // Not allowed
+accountEmail= "hc@hc.com"
+accountPassword = "21324"
+accountCity =" Bangalore"
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity]);
