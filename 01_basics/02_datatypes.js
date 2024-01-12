a0734e2670278ca46bdadad8f83c866c7a66c8ff
Harrsha T <142029312+harrshat@users.noreply.github.com>
2024-01-12 06:01:03 +0000

    Conversion Operation

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -1,2 +1,19 @@
 "use strict"; //treat all JS code as newer version
 alert("Hello!")
+
+let name = "acbd"
+let age = 18
+let isLoggedIn = false
+
+//number => 2 pow 53
+//bigint
+//boolean=>true/false
+//null => standalone value
+//undefined =>no value assigned yet
+//symbol 
+
+//object
+
+console.log(typeof "Harrsha");
+console.log(typeof age);
+console.log(typeof null);
