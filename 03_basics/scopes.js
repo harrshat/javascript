adc4fe44bb5d20db50c01f0f373ef6861f1758ea
Harrsha T <142029312+harrshat@users.noreply.github.com>
2024-01-15 13:03:04 +0000

    Functions

--- 03_basics/scopes.js
+++ 03_basics/scopes.js
@@ -6,6 +6,27 @@ if(true){
     var c = 30
 }
 
-console.log(a);
-console.log(b);
-console.log(c);
+// console.log(a);
+// console.log(b);
+//console.log(c);
+
+//Scope level and mini hoisting
+function one() {
+    const userName = "harrsha";
+
+    function two() {
+        const website = "youtube"
+        console.log(userName);
+    }
+    // console.log(website);
+    two()
+}
+one()
+
+//Expressions and hoisting
+const addTwo = function(num){
+    return num + 2;
+}
+
+addTwo(5);
+
